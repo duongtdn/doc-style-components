5f9c2dcf3c38256377ee62898f47df30189134a3
duongtdn <duongtdn@gmail.com>
2024-02-15 14:52:00 +0700

    Add StyledInput

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import P from "./P";
 import Img from "./Img";
 import Code from "./Code";
 import CheckBox from "./CheckBox";
+import StyledInput from "./StyledInput";
 
-export { CheckBox };
+export { CheckBox, StyledInput };
 
 export default {
   P,
   Img,
   Code,
   CheckBox,
+  StyledInput,
 };
